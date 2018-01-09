c614576ca48a427a17bd25f0ef073c365d5bf6d4
TiagoJSMagalhaes <Crakylps@gmail.com>
2018-01-09 17:06:21 +0000

    now listing users

--- Resgister/views.py
+++ Resgister/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import User
 
 # Create your views here.
 def list(request):
     All_users = User.objects.all()
+    template = loader.get_template("list.html")
+    context = {
+        "All_users": All_users
+    }
 
-    html = "<h1>User List:</h1>"
-
-    for i in All_users:
-        html += "<p>Username:" + i.name + " Email:" + i.email + "</p>"
-
-    return HttpResponse(html)
+    return HttpResponse(template.render(context,request))
 
 def index(request):
     html = ""
